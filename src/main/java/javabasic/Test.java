76c568d22d53b0e920d584e58f94307d719c5289
fzj <fzj0522@outlook.com>
2018-08-18 15:28:10 +0800

    update

--- src/main/java/javabasic/Test.java
+++ src/main/java/javabasic/Test.java
@@ -1,5 +1,9 @@
 package main.java.javabasic;
 
+import main.java.zutil.ListNode;
+
+import java.util.*;
+
 /**
  * @Desc
  * @Author Fan Zejun E-mail:fzj0522@outlook.com
@@ -8,7 +12,28 @@ package main.java.javabasic;
 public class Test {
 
     public static void main(String[] args){
-       Test t = new Test();
-        System.out.println(Test.class.isInstance(t));
+        int[] nums = new int[]{1,4,6,2,3};
+        ListNode head = ListNode.generateList(nums);
+
+        List<ListNode> list = new ArrayList<>();
+        while (head!=null){
+            list.add(head);
+            head = head.next;
+        }
+        Comparator myComparator = new MyComparator();
+        Collections.sort(list,myComparator);
+        for(ListNode node : list){
+            System.out.print(node.val);
+        }
+    }
+
+
+}
+
+class MyComparator implements Comparator{
+    public int compare(Object a, Object b){
+        if(((ListNode)a).val<((ListNode)b).val) return -1;
+        else if(((ListNode)a).val == ((ListNode)b).val) return 0;
+        else return 1;
     }
 }
